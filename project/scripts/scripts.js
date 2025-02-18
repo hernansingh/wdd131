93b2822163fc6f0bfe2dd0fa6558109aee2fdebf
hernanjoelsingh <hernanjoelsingh@gmail.com>
2025-02-18 16:23:37 -0300

    smooth

--- project/scripts/scripts.js
+++ project/scripts/scripts.js
@@ -5,3 +5,19 @@ menuButton.addEventListener('click', function() {
     menuButton.classList.toggle('open');
     nav.classList.toggle('open');
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: "smooth"
+            });
+        }
+    });
+});
